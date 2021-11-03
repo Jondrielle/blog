3ea92ba78f508b9aa07e0e8b79a5e52ee36bbd17
Jondrielle <Jondriellewilson@gmail.com>
2021-11-03 18:40:05 -0500

    First upload to our project

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text,StyleSheet,View} from "react-native";
+
+const IndexScreen = () => {
+	return <View>
+		<Text>Index Screen</Text>
+	</View>
+}
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
